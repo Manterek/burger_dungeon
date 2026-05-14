99ee68e13747081eb7bd1452ea9973cb064067dd
Manterek <patrykkowal08@gmail.com>
2026-05-14 06:32:42 +0200

    Delete main directory

--- main/dev-config.js
+++
@@ -1,4 +0,0 @@
-export const DEV_MODE_VISIBILITY_ENABLED = false;
-export const DEFAULT_DEV_MODE_ENABLED = false;
-export const DEV_MULTIPLAYER_MODE = "local";
-export const MULTIPLAYER_BACKEND_URL = "";
